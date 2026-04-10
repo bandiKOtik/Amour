eedb2fc5ab1476f5cc7324c211b62c9f6adb01bd
kuamorg <anon12341242345@gmail.com>
2026-04-11 01:14:11 +0300

    ботек2

--- Content.Packaging/ServerPackaging.cs
+++ Content.Packaging/ServerPackaging.cs
@@ -129,6 +129,7 @@ public static class ServerPackaging
         "Npgsql",
         "Microsoft",
         "Concentus",
+        "Discord.Net",
     };
 
     private static readonly List<string> ServerNotExtraAssemblies = new()
